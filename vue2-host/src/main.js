b9ee1345ca4099cd5a82d7a258a593c19181f0e5
Kenta Hamanaka <hamanaka@fixpoint.co.jp>
2022-03-08 15:04:49 +0900

    Load from host

--- vue2-host/src/main.js
+++ vue2-host/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import Vue2SfcLib from "vue2-sfc-lib";
+
+Vue.config.productionTip = false;
+
+Vue.use(Vue2SfcLib);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
