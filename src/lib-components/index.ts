c5ac1d6a9ee7268961320adb44b10388bebfcbc2
Kenta Hamanaka <hamanaka@fixpoint.co.jp>
2022-03-08 14:54:03 +0900

    Move into dirctory

--- src/lib-components/index.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as Vue2SfcLibSample } from './vue2-sfc-lib-sample.vue';
---
+++ vue2-sfc-lib/src/lib-components/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vue2SfcLibSample } from "./vue2-sfc-lib-sample.vue";
+export { default as TestComponent } from "./test-component.vue";
