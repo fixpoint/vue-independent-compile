06e26e53d5e34f6b16620c057edb15ffaf944458
Kenta Hamanaka <hamanaka@fixpoint.co.jp>
2022-03-08 12:18:29 +0900

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue2SfcLibSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  Vue2SfcLibSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-sfc-lib-sample />
+  </div>
+</template>
