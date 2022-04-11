1d4eb90e331dd7a93ede67bbc72b0cb29a87deca
Kenta Hamanaka <hamanaka@fixpoint.co.jp>
2022-04-11 20:12:45 +0900

    Import from sfc-lib-b

--- router-host/src/router/index.ts
+++ router-host/src/router/index.ts
@@ -19,7 +19,12 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(
+        /* webpackChunkName: "about" */
+        // eslint-disable-next-line
+        // @ts-ignore
+        "../../../sfc-lib-b/dist/sfc-lib-b.es.js"
+      ).then((module) => module.AboutView),
   },
 ];
 
